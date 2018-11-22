78a490ee67e399e916ff54b73a362cefc15fb469
JuaniTopo <juana.der@gmail.com>
2018-11-22 17:50:30 -0300

    cambio el user

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bm67oxi)ab7p#j@zr*)je61mp2+0@!dj7&j6ctbsj*l9pncn82'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS =  ['127.0.0.1', 'JuaniTopo.pythonanywhere.com']
 
 
 # Application definition
